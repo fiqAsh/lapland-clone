27f950dcc724eb0fd19c99b1a847642d4ce4974c
fiqAsh <ashfiq625@gmail.com>
2024-05-28 23:14:12 +0600

    woking on slider

--- practice.js
+++ practice.js
@@ -10,3 +10,27 @@ adiv.addEventListener('click',redirect)
 function redirect(){
   window.location.href='home.html'
 }
+
+
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.classList.add('active');
+        } else {
+            slide.classList.remove('active');
+        }
+    });
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    showSlide(currentIndex);
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
